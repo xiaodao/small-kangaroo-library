e46efce83758eae5c119f1b4a5ff2aed44d31752
jacky li <veryfaint@gmail.com>
2014-04-25 20:04:39 +1000

    use AVFoundation to scan barcode

--- small_kangaroo_library/Controller/ScanViewController.m
+++ small_kangaroo_library/Controller/ScanViewController.m
@@ -5,34 +5,81 @@
 //  Created by Jacky Li on 23/4/14.
 //  Copyright (c) 2014 SmallKangarooLibrary. All rights reserved.
 //
+#import <AVFoundation/AVMediaFormat.h>
+#import <AVFoundation/AVCaptureInput.h>
+#import <AVFoundation/AVCaptureOutput.h>
+#import <AVFoundation/AVCaptureDevice.h>
+#import <AVFoundation/AVCaptureVideoPreviewLayer.h>
+#import <AVFoundation/AVMetadataObject.h>
 
 #import "ScanViewController.h"
 
-@interface ScanViewController ()
-
+@interface ScanViewController () <AVCaptureMetadataOutputObjectsDelegate>
+@property(nonatomic, strong) AVCaptureSession *session;
+@property(nonatomic, strong) AVCaptureDevice *device;
+@property(nonatomic, strong) AVCaptureDeviceInput *input;
+@property(nonatomic, strong) AVCaptureMetadataOutput *output;
+@property(nonatomic, strong) AVCaptureVideoPreviewLayer *prevLayer;
 @end
 
 @implementation ScanViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  _session = [[AVCaptureSession alloc] init];
+  _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+  NSError *error = nil;
+
+  _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
+  if (_input) {
+    [_session addInput:_input];
+  } else {
+    NSLog(@"Error: %@", error);
+  }
+
+  _output = [[AVCaptureMetadataOutput alloc] init];
+  [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+  [_session addOutput:_output];
+
+  _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
+
+  _prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+  _prevLayer.frame = self.view.bounds;
+  _prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+  [self.view.layer addSublayer:_prevLayer];
+
+  [_session startRunning];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
+  NSString *detectionString = nil;
+  NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
+          AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
+          AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
+
+  for (AVMetadataObject *metadata in metadataObjects) {
+    for (NSString *type in barCodeTypes) {
+      if ([metadata.type isEqualToString:type]) {
+        detectionString = [(AVMetadataMachineReadableCodeObject *) metadata stringValue];
+        break;
+      }
     }
-    return self;
-}
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    if (detectionString != nil) {
+      NSLog(@"ISBN: %@", detectionString);
+      [_session stopRunning];
+//      [self.presentingViewController dismissViewControllerAnimated:YES completion:^{}];
+      break;
+    }
+  }
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
